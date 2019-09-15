51c2418a5c9395727d47622870b9f4a939c51384
Linh Phan <linhphan0108@gmail.com>
2019-09-15 16:45:39 +0700

    - add adapterdelegates4 and configure it's base classes

---
+++ app/src/main/java/com/example/foreverfreedictionary/ui/adapter/viewholder/LoadMoreViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.foreverfreedictionary.ui.adapter.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+
+class LoadMoreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bindView(){
+
+    }
+}
---
+++ app/src/main/java/com/example/foreverfreedictionary/ui/model/Entity.kt
@@ -0,0 +1,7 @@
+package com.example.linh.vietkitchen.ui.model
+
+/**
+ * Created by linh on 6/1/2016.
+ * this is a model class base for using in AdapterDelegate
+ */
+abstract class Entity(val id: String? = "", var isSelected: Boolean = false)
